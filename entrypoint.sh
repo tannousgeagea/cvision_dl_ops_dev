b5cb3f54444991f0d43c29452da792f04bceeaa0
Tannous Geagea <tannous.geagea@wasteant.com>
2025-05-22 08:22:41 +0200

    remove collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 /bin/bash -c "python3 /home/$user/src/cvision_ops/manage.py makemigrations"
 /bin/bash -c "python3 /home/$user/src/cvision_ops/manage.py migrate"
 /bin/bash -c "python3 /home/$user/src/cvision_ops/manage.py create_superuser"
-/bin/bash -c "python3 /home/$user/src/cvision_ops/manage.py collectstatic"
+# /bin/bash -c "python3 /home/$user/src/cvision_ops/manage.py collectstatic"
 
 sudo -E supervisord -n -c /etc/supervisord.conf
