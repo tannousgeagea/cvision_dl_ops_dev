85b6419e50d80596489e40b6a27df876b88552e0
Tannous Geagea <tannousgeagea@hotmail.com>
2025-10-19 12:22:52 +0200

    changed static files

--- cvision_ops/staticfiles/unfold/js/select2.init.js
+++ cvision_ops/staticfiles/unfold/js/select2.init.js
@@ -8,13 +8,17 @@
         return;
       }
 
+      if ($(element).hasClass("select2-hidden-accessible")) {
+        return;
+      }
+
       $(element).select2();
     });
 
     return this;
   };
 
-  $.fn.djangoAdminSelect2 = function () {
+  $.fn.djangoFilterSelect2 = function () {
     $.each(this, function (i, element) {
       $(element).select2({
         ajax: {
@@ -36,13 +40,6 @@
   $(function () {
     $(".unfold-admin-autocomplete.admin-autocomplete").djangoCustomSelect2();
 
-    $(".admin-autocomplete")
-      .not(".unfold-admin-autocomplete")
-      .not("[name*=__prefix__]")
-      .djangoAdminSelect2();
-  });
-
-  document.addEventListener("formset:added", (event) => {
-    $(event.target).find(".admin-autocomplete").djangoAdminSelect2();
+    $(".unfold-filter-autocomplete.admin-autocomplete").djangoFilterSelect2();
   });
 }
